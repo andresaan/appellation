e3b10a9b3aa38d3bb46f7bbd4816c44d1e7e0222
Andre Sanchez <aasanchez437@gmail.com>
2023-09-15 11:28:26 -0500

    Fixed redirect error during auth

--- Appellation/Controllers/Login.cs
+++ Appellation/Controllers/Login.cs
@@ -5,11 +5,13 @@ namespace Appellation.Controllers
 {
     public class Login : Controller
     {
+        // User interacted controller and the button in view will need to send user to my endpoint
         public IActionResult Index()
         {
             return View();
         }
 
+        // Move to seperate entity auth controller
         public IActionResult Challenge()
         {
             var properties = new AuthenticationProperties()
